b3a0eb35cb256cf43485ace1e7857403bd60ae4a
Guezin <leandroguezinjunior@gmail.com>
2020-04-13 10:25:28 -0300

    Project CRUD, Likes.

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+  const repository = repositories.find(repo => repo.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+  
+  repository.url = url;
+  repository.title = title;
+  repository.techs = techs;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.findIndex(repo => repo.id === id);
+
+  if (repository < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repository, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.find(repo => repo.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+});
+
+module.exports = app;
